10d3df5949ca6d47e15f76094648d899bc128a28
liushuai <770722922@qq.com>
2020-05-15 15:13:28 +0800

    feature: make different id-type

--- MakeEvent.hpp
+++ MakeEvent.hpp
@@ -30,21 +30,31 @@
 
 
 #define _MAKE_EVENT_DEF_ADD(name, ...) \
-        using name##EventID_t = unsigned int; \
+        struct name##CbID_t { \
+            name##CbID_t(unsigned int value = 0) : value(value) {} /* NOLINT(google-explicit-constructor) */ \
+            unsigned int value; \
+            name##CbID_t operator++() { \
+                value++; \
+                return *this; \
+            } \
+            bool operator==(const name##CbID_t& other) const { \
+                return value == other.value; \
+            } \
+        }; \
         using name##Cb##_t = std::function<void(__VA_ARGS__)>; \
-        using name##Cb##Pair_t = std::pair<name##EventID_t, std::function<void(__VA_ARGS__)>>; \
+        using name##Cb##Pair_t = std::pair<name##CbID_t, std::function<void(__VA_ARGS__)>>; \
         private: \
-        name##EventID_t _cbs##name##ID##Count = 0; \
+        name##CbID_t _cbs##name##ID##Count = 0; \
         std::vector<name##Cb##Pair_t> _cbs##name; \
 
 #define _MAKE_EVENT_FUN_ADD(name, ...) \
         public: \
-        inline name##EventID_t add##name##Cb(name##Cb##_t cb) { \
+        inline name##CbID_t add##name##Cb(name##Cb##_t cb) { \
             auto id = ++_cbs##name##ID##Count; \
             _cbs##name.emplace_back(std::make_pair(id, std::move(cb))); \
             return id; \
         } \
-        inline name##EventID_t remove##name##CbById(name##EventID_t id) { \
+        inline name##CbID_t remove##name##CbById(name##CbID_t id) { \
             for (auto it = _cbs##name.cbegin(); it != _cbs##name.cend();) { \
                 const auto& item = *it; \
                 if (item.first == id) { \
